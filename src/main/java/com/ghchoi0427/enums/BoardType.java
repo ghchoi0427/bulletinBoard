64e7d4b4346d33c940ece07d8862e911d7900814
ghchoi0427 <ghchoi0427@gmail.com>
2021-04-01 10:18:42 +0900

    feat: BoardType 생성, 구현

---
+++ src/main/java/com/ghchoi0427/enums/BoardType.java
@@ -0,0 +1,42 @@
+package com.ghchoi0427.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+@Builder
+public class BoardType {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long idx;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private BoardType boardType;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updatedDate;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+}
