53f714195d78b64b2aa23c87da9f28c174e0965a
ghchoi0427 <ghchoi0427@gmail.com>
2021-04-01 10:50:32 +0900

    feat: BoardService 클래스 구현

---
+++ src/main/java/com/ghchoi0427/repository/BoardService.java
@@ -0,0 +1,22 @@
+package com.ghchoi0427.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    public Page<Board> findBoardList(Pageable pageable) {
+        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize());
+        return boardRepository.findAll(pageable);
+    }
+
+    public Board findBoardByIdx(Long idx) {
+        return boardRepository.findById(idx).orElse(new Board());
+    }
+}
