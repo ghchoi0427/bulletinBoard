cbd2c3dbc806c611b780f413beac9d1e2b54746b
ghchoi0427 <ghchoi0427@gmail.com>
2021-04-02 10:15:51 +0900

    fix: BoardType 수정

--- src/main/java/com/ghchoi0427/domain/enums/BoardType.java
+++ src/main/java/com/ghchoi0427/domain/enums/BoardType.java
@@ -1,42 +1,16 @@
 package com.ghchoi0427.domain.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+public enum BoardType {
+    notice("공지사항"),
+    free("자유게시판");
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+    private String value;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table
-@Builder
-public class BoardType {
+    BoardType(String value) {
+        this.value = value;
+    }
 
-    @Id
-    @Column
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public long idx;
-
-    @Column
-    private String title;
-
-    @Column
-    private String content;
-
-    @Column
-    @Enumerated(EnumType.STRING)
-    private BoardType boardType;
-
-    @Column
-    private LocalDateTime createdDate;
-
-    @Column
-    private LocalDateTime updatedDate;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    private User user;
+    public String getValue() {
+        return this.value;
+    }
 }
