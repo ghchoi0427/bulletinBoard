9207bb1ba9cbbb86123a01410ccce9e88b4a4de2
ghchoi0427 <ghchoi0427@gmail.com>
2021-04-04 12:49:21 +0900

    refactor: 공백 제거

--- src/main/java/com/ghchoi0427/contoller/BoardController.java
+++ src/main/java/com/ghchoi0427/contoller/BoardController.java
@@ -22,7 +22,6 @@ public class BoardController {
     @GetMapping({"", "/"})
     public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model) {
         model.addAttribute("board", boardService.findBoardByIdx(idx));
-
         return "/board/form";
     }
 
